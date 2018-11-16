bd74a33e39cf0c50196cb641a2732d59ecee053a
l0g1cb0mb <34043228+l0g1cb0mb@users.noreply.github.com>
2018-11-17 00:29:24 +0100

    Program ktory umozliwia zamkniecie systemu za okreslona ilosc minut. Umozliwia rowniez anulowanie tej akcji.

---
+++ Shutdown/shutdown.py
@@ -0,0 +1,26 @@
+import os
+
+def shutdown():
+    min = int(input("Za ile minut ma nastąpić wyłączenie komputera?\n"))
+    sec = 60 * min
+    os.system(f"shutdown -s -t {sec}")
+
+def cancel_shutdown():
+    os.system("shutdown -a")
+
+def main():
+    while(True):
+        print("0. Wyjście", "1. Wyłącz komputer", "2. Anuluj wyłączanie komputera", sep="\n")
+        x = int(input("Wybierz co chcesz zrobić: "))
+
+        if 0 <= x <= 2:
+            if x == 0:
+                break
+            elif x == 1:
+                shutdown()
+                os.system("cls")
+            elif x == 2:
+                cancel_shutdown()
+                os.system("cls")
+				
+main()
