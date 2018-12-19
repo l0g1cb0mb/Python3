29439f0cbefcd0489915615d63e1f4fa9b6761b5
l0g1cb0mb <34043228+l0g1cb0mb@users.noreply.github.com>
2018-12-19 20:29:25 +0100

    little changes

--- Shutdown/shutdown.py
+++ Shutdown/shutdown.py
@@ -1,7 +1,7 @@
 import os
 
 def shutdown():
-    sec = int(input("Za ile minut ma nastąpić wyłączenie komputera?\n"))
+    sec = int(input("How many minutes to shutdown the computer?\n"))
     min = 60 * sec
     os.system(f"shutdown -s -t {min}")
 
@@ -10,17 +10,18 @@ def cancel_shutdown():
 
 def main():
     while(True):
-        print("0. Wyjście", "1. Wyłącz komputer", "2. Anuluj wyłączanie komputera", sep="\n")
-        x = int(input("Wybierz co chcesz zrobić: "))
+        print("0. Quit", "1. Shutdown", "2. Cancel the shutdown", sep="\n")
+        choice = int(input("Your choice: "))
 
-        if 0 <= x <= 2:
-            if x == 0:
-                break
-            elif x == 1:
+        if 0 <= choice <= 2:
+            if choice == 0:
+                return
+            elif choice == 1:
                 shutdown()
                 os.system("cls")
-            elif x == 2:
+            elif choice == 2:
                 cancel_shutdown()
                 os.system("cls")
-				
-main()
+                
+if __name__ == "__main__":				
+    main()
