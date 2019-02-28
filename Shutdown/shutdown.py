abfd53faa26e8b872abbf3ff80735af9ca386e66
unknown <s085251@student.tu.kielce.pl>
2019-02-28 09:51:23 +0100

    Added try...except block for ValueErrors

--- Shutdown/shutdown.py
+++ Shutdown/shutdown.py
@@ -1,27 +1,41 @@
 import os
 
-def shutdown():
+def minutes():
     sec = int(input("How many minutes to shutdown the computer?\n"))
-    min = 60 * sec
-    os.system(f"shutdown -s -t {min}")
+    return 60 * sec
+
+def shutdown():
+    try:
+        min = minutes()
+    except ValueError as verr:
+        print("The input given must be the type of int")
+    else:
+        os.system(f"shutdown -s -t {min}")
 
 def cancel_shutdown():
     os.system("shutdown -a")
 
+def user_choice():
+    choice = int(input("Your choice: "))
+    return choice
+
 def main():
     while(True):
         print("0. Quit", "1. Shutdown", "2. Cancel the shutdown", sep="\n")
-        choice = int(input("Your choice: "))
-
-        if 0 <= choice <= 2:
-            if choice == 0:
-                return
-            elif choice == 1:
-                shutdown()
-                os.system("cls")
-            elif choice == 2:
-                cancel_shutdown()
-                os.system("cls")
+        try:
+           choice = user_choice()
+        except ValueError as verr:
+            print("Invalid option, try again!")
+        else:
+            if 0 <= choice <= 2:
+                if choice == 0:
+                    return
+                elif choice == 1:
+                    shutdown()
+                    os.system("cls")
+                elif choice == 2:
+                    cancel_shutdown()
+                    os.system("cls")
                 
 if __name__ == "__main__":				
     main()
